32ff075732314090c30ff66be78c06bbe76de0c7
GowthamAbi <gowtham2131ece@gmail.com>
2026-02-25 07:47:46 +0530

    db

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ dotenv.config()
 connectDB().catch((error)=>{console.log("Error in MangoDB Connecting in Server page",error)})
 
 app.use("/",itemRoute)
+app.use("/",itemRoute)
 
 app.listen(3000,(req,res)=>(
     console.log("the server is running on http://Localhost:3000")
